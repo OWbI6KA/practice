4fd895a4b51a0969eab491b996d0e9ceb7d55900
samoy <samoylenkk@inbox.ru>
2022-06-12 14:25:47 +0300

    Updates

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import eel
+import scripts.zoomVideoDownloader
+import scripts.YandexDiskUploader
+from scripts.YandexDiskUploader import YandexDiskUploader
+from scripts.zoomVideoDownloader import ZoomDownloader
+
 from scripts import get_data
 eel.init("web")
 
+def get_data(userDataFrom, userDataTo, strDataFrom='', strDataTo=''):
+    dataFrom = userDataFrom.split('-')
+    dataTo = userDataTo.split('-')
+    dataFrom.reverse()
+    dataTo.reverse()
+    for i in range(3):
+        strDataFrom += dataFrom[i] + '/'
+        strDataTo += dataTo[i] + '/'
+    return strDataFrom[:-1], strDataTo[:-1]
 
 @eel.expose
-def recv_data(key, folderWay, dataFrom, dataTo, file):
+def main_script(tokenYDisk, tokenZoom, dataFrom, dataTo, file, uploadType, resourceType):
     myDataFrom, myDataTo = get_data(userDataFrom=dataFrom, userDataTo=dataTo)
-    print(myDataFrom, myDataTo)
-    print(file)
+
+    if resourceType == "zoom":
+        if uploadType == "typeLinks":
+            print(ZoomDownloader.downloadVideosToFolder(tokenZoom, dateFrom=dataFrom, dateTo=dataTo, pathToFileWithAccounts=file))
+            disk = YandexDiskUploader(token=tokenYDisk)
+            print(disk.UploadVideosFromFolder('temp'))
+        else:
+            print(ZoomDownloader.uploadVideosToDisk(tokenYDisk, tokenZoom, dateFrom=dataFrom, dateTo=dataTo, pathToFileWithAccounts=file))
+    else:
+        print(0)
+
 
 
 eel.start('main.html', size=(800, 900))
